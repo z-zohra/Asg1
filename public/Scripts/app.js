3e59e08f794f9937604901e0b55e1e8d5f9ed286
z-zohra <zohra.5624@gmail.com>
2022-10-21 23:54:47 -0400

    --firstcommit

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,10 +1,24 @@
-//IIFEE -- IMediately Invoked Function expression
+// IIFE -- Immediately Invoked Function Expression
 (function(){
 
-   function Start() {
+   function Start()
+   {
+       console.log("App Started...");
 
-    console.log("App Started....");
+       let deleteButtons = document.querySelectorAll('.btn-danger');
+       
+       for(button of deleteButtons)
+       {
+           button.addEventListener('click', (event)=>{
+               if(!confirm("Are you sure?")) 
+               {
+                   event.preventDefault();
+                   window.location.assign('/book-list');
+               }
+           });
+       }
    }
 
-   window.addEventListener("load, Start");
+   window.addEventListener("load", Start);
+
 })();
